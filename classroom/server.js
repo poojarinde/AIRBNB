14d0430354727dc428d09a8c9b939c249f5e8215
poojarinde <poojarinde@gmail.com>
2024-06-29 17:26:15 +0530

    changes about review

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,10 @@ const app=express();
 const users=require("./routes/user.js");
 const posts=require("./routes/post.js");
 
+app.get("/getcookies",(req,res)=>{
+   res.send("Sent you some cookies");
+});
+
 app.get("/",(req,res)=>{
     res.send("Hi,I am root");
 });
--- routes/review.js
+++ routes/review.js
@@ -1,10 +1,22 @@
 const express=require("express");
-const router=express.Router();
+const router=express.Router({mergeParams:true});
 const wrapAync=require("../utils/wrapAsync.js");
 const ExpressError=require("../utils/ExpressError.js");
 const {reviewSchema}=require("../schema.js");
 const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const validateReview=(req,res,next) => {
+    
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message).join(",");
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+        next();
+    }
+};
 
 router.post("/",validateReview,wrapAsync(async(req,res)=>{
     let listing=await Listing.findById(req.params.id);
